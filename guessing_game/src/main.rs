16211efd18850a7ffe224e315d675a3065b18eb1
Pajer3 <musieatsbeha633@gmail.com>
2025-09-14 14:48:52 +0200

    Refactor input handling in guessing game to improve user experience

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,22 +4,32 @@ use std::io;
 
 fn main() {
     let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
-    let mut guess: String = String::new();
+    
+    loop {
+        let mut guess: String = String::new();
+        println!("Guess the number!");
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess: u32 = match guess
+            .trim()
+            .parse()
+        {
+            Ok(num) => num,
+            Err(_) => continue,
+        };7
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess: u32 = guess
-        .trim()
-        .parse()
-        .expect("Please type a number!");
-
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        println!("You guessed: {guess}");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
