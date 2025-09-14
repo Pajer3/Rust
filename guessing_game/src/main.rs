93455d86a962567e0f3fe20712c1e5d1a03eebf8
Pajer3 <musieatsbeha633@gmail.com>
2025-09-14 14:32:00 +0200

    Refactor guessing game logic and improve user prompts

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,19 +1,25 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
     let mut guess: String = String::new();
-
-    println!("Guess the number!");
-    println!("Please input your guess.");
-
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
-    println!("The secret number is: {secret_number}");
+    let guess: u32 = guess
+        .trim()
+        .parse()
+        .expect("Please type a number!");
 
+    println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
