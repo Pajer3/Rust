6cb69288891f4874ca1287afb8686d1fbe558f2b
Pajer3 <musieatsbeha633@gmail.com>
2025-10-01 12:51:10 +0200

    Refactor test module structure and update test function names for clarity

--- y/tests/demo.rs
+++ y/tests/demo.rs
@@ -1,10 +1,25 @@
-use y;
+#[cfg(test)]
+mod tests {
+    use y::front_of_house::hosting::{add_to_waitlist, seat_at_table};
+    use y::front_of_house::serving::{take_order, serve_order, take_payment};
+    use y::back_of_house::cooking::{cook_order, fix_incorrect_order};
+    use y::back_of_house::management::handle_complaint;
 
-#[test]
-fn try_out_paths() {
-    y::front_of_house::hosting::add_to_waitlist();
-    y::front_of_house::hosting::seat_at_table();
-    y::front_of_house::serving::take_order();
-    y::front_of_house::serving::serve_order();
-    y::front_of_house::serving::take_payment();
+    #[test]
+    fn try_front_paths() {
+        add_to_waitlist();
+        seat_at_table();
+        take_order();
+        serve_order();
+        take_payment();
+    }
+
+
+    #[test]
+    fn try_back_paths() {
+        cook_order();
+        fix_incorrect_order();
+        handle_complaint();
+    }
+
 }
